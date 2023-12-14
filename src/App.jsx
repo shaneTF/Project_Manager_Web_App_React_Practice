4ecd4a3240b9ff97f8c7f7f259d6359eb79cd153
Shane Franklin <shanefranklin148@gmail.com>
2023-12-14 13:39:20 -0500

    First commit. Add side bar for projects.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ProjectSideBar from "./components/ProjectSideBar";
+
+function App() {
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSideBar />
+    </main>
+  );
+}
+
+export default App;
